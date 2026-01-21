a866a633e105615ca42a4b643c6f1d44e5464556
Student_08 <Student_08@DESKTOP-ESEQ4S2>
2026-01-21 15:10:19 +0300

    Create payment procesing system

---
+++ src/payment/Payment.kt
@@ -0,0 +1,27 @@
+sealed class PaymentResult{
+    data class Success(val id:String) : PaymentResult()
+    data class Error(val reason:String) : PaymentResult()
+    object Proccessing : PaymentResult()
+}
+enum class CardType{
+    VISA,MASTERCARD,MIR,UNKNOWN
+}
+data class Payment(
+    val card:String,
+    val sum:Int,
+    val type: CardType=CardType.UNKNOWN
+)
+class PaymentValidator{
+    fun check(payment:Payment): Boolean{
+        return when(payment.type){
+            CardType.VISA->payment.card.length==16
+                    && payment.card.startsWith("4")
+            CardType.MASTERCARD->payment.card.length==16
+                    && payment.card.startsWith("5")
+            CardType.MIR -> payment.card.length==16
+                    && payment.card.startsWith("2")
+            CardType.UNKNOWN -> payment.card.length==16
+
+        }&& payment.sum > 0
+    }
+}
---
+++ src/payment/PaymentValidator.kt
@@ -0,0 +1,21 @@
+class PaymentProccessor{
+    private val validator=PaymentValidator()
+    fun pay(payment: Payment): PaymentResult{
+        if (!validator.check(payment)){
+            return PaymentResult.Error("Ошибка валидации")
+        }
+        return when (payment.type) {
+            CardType.VISA -> PaymentResult.Success("VISA-${System.currentTimeMillis()}")
+            CardType.MASTERCARD -> PaymentResult.Proccessing
+            CardType.MIR -> PaymentResult.Success("MIR-${System.currentTimeMillis()}")
+            CardType.UNKNOWN -> PaymentResult.Error("Неизвестный тип карты")
+        }
+    }
+    fun show(result: PaymentResult){
+        when (result) {
+            is PaymentResult.Success -> println("Успех: ${result.id}")
+            is PaymentResult.Error -> println("Ошибка: ${result.reason}")
+            PaymentResult.Proccessing -> println("В обработке...")
+        }
+    }
+}
